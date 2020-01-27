7ee157900163bcfecd816cba1ea884da9b6251aa
sircferrer <ferrercarballo@gmail.com>
2020-01-27 13:44:56 +0100

    grid styles finish filer by menu and input

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -7,7 +7,29 @@ const grid = new Muuri(".grid", {
 });
 
 window.addEventListener('load', () => {
+    grid.refreshItems().layout();
     document.getElementById("grid").classList.add("loadImg")
+    //Agregar los listeners de los links para filtrar por data-category
+    const links = document.querySelectorAll("#header__options a");
+    links.forEach((link) => {
+        link.addEventListener("click", (ev) => {
+            ev.preventDefault();
+            links.forEach((link) => link.classList.remove("active"))
+            ev.target.classList.add("active")
+            const category = ev.target.innerHTML;
+            category === "Todos" ? grid.filter("[data-category]") : grid.filter(`[data-category= "${category}"]`)
+
+        })
+
+    })
+
+    //Agregar los listeners para la barra búsqueda
+
+    document.querySelector("#search-menu").addEventListener("input", (ev) => {
+        const search = ev.target.value;
+        grid.filter((item) => item.getElement().dataset.labels.includes(search))
+
+    })
 })
 
 
